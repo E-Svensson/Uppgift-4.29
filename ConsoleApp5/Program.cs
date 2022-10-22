995a497cbf129cccea04d567222aa7ff7a641ddb
E-Svensson <edvin.sven123@gmail.com>
2022-10-22 20:46:43 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bestäm triangelns sidlängd: "); int sidlängd = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < sidlängd; i++)
+            {
+                Console.WriteLine();
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
